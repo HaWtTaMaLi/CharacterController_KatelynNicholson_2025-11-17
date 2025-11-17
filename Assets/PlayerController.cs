9642be90d5beb03c2020bd76d4c8460ea189dfd4
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-11-17 12:40:37 -0400

    set my stuff back up from restart

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject Player;
+
+    public int speed = 5;
+    public float jumpHeight = 1.5f;
+    public float gravity = -9f;
+
+    public CharacterController playerController; //get component
+    public bool isGrounded = true;
+    public Vector3 playerVelocity;
+
+    [Header("Input Actions")]
+    public InputActionReference Move; //vector2
+    public InputActionReference Look; //vector2
+    public InputActionReference Jump; //button
+    public InputActionReference Sprint; //button
+    public InputActionReference Interact; //button
+    public InputActionReference Crouch; //button
+
+    public void Update()
     {
-        
+        isGrounded = playerController.isGrounded;
+
+        float x = Input.GetAxis("Horizontal"); //W && S
+        float z = Input.GetAxis("Vertical"); //A && D
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        Player.transform.position += move * speed * Time.deltaTime; //change transform to Move character controller methods
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
